4ec9957a1ddedb2476609c1e84068c902803e7e4
ezequias2d <ezequiasmoises@gmail.com>
2021-11-14 23:20:40 -0300

    Implements IDisposable in ITexture2D, add IsDisposed flag and change size property to use Size struct

--- src/Peridot/ITexture2D.cs
+++ src/Peridot/ITexture2D.cs
@@ -3,12 +3,23 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-using System.Numerics;
+using System.Drawing;
 
 namespace Peridot
 {
-    public interface ITexture2D
+    /// <summary>
+    /// Represents a 2D texture to use in <see cref="SpriteBatch{TTexture}"/>.
+    /// </summary>
+    public interface ITexture2D : IDisposable
     {
-        public Vector2 Size { get; }
+        /// <summary>
+        /// A bool indicating whether this instance has been disposed.
+        /// </summary>
+        public bool IsDisposed { get; }
+
+        /// <summary>
+        /// The total size of the texture.
+        /// </summary>
+        public Size Size { get; }
     }
 }
