46cb710c8e8871f6487b56fdb11c240912de3fef
ezequias2d <ezequiasmoises@gmail.com>
2021-11-14 23:39:07 -0300

    Add ITextRenderer and IFont interface

---
+++ src/Peridot/IFont.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Peridot
+{
+    /// <summary>
+    /// Represents a font to use in <see cref="SpriteBatch{TTexture}"/>.
+    /// </summary>
+    public interface IFont : IDisposable
+    {
+        /// <summary>
+        /// Gets the size of a string when rendered in this font.
+        /// </summary>
+        /// <param name="text">The text to measure.</param>
+        /// <param name="fontSize">The font size to measure.</param>
+        /// <returns>The size of <paramref name="text"/> rendered with a <see cref="ITextRenderer"/> 
+        /// with <paramref name="fontSize"/> font size.</returns>
+        public Vector2 MeasureString(string text, int fontSize);
+    }
+}
