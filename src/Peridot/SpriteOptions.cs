2610a3a7fb2a4801187e6bf83645ca1a2875dd22
ezequias2d <ezequiasmoises@gmail.com>
2022-03-19 13:09:20 -0300

    Adds SpriteOptions enum

---
+++ src/Peridot/SpriteOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace Peridot
+{
+    /// <summary>
+    /// Defines sprite options.
+    /// </summary>
+    [Flags]
+    public enum SpriteOptions
+    {
+        /// <summary>
+        /// Nothing is applied in sprite rendering.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Horizontal flip is applied in sprite rendering.
+        /// </summary>
+        FlipHorizontally,
+        /// <summary>
+        /// Vertical flip is applied in sprite rendering.
+        /// </summary>
+        FlipVertically,
+    }
+}
