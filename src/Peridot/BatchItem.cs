38f973eca54e30767f68eb9a98cd6af90b3b0743
ezequias2d <ezequiasmoises@gmail.com>
2022-03-19 18:21:24 -0300

    Refactor: Reuses constructor of BatchItem

--- src/Peridot/BatchItem.cs
+++ src/Peridot/BatchItem.cs
@@ -8,42 +8,8 @@ namespace Peridot
 {
     public struct BatchItem
     {
-        public BatchItem(
-                Vector2 textureSize,
-                Vector2 position,
-                Rectangle sourceRectangle,
-                Color color,
-                float rotation,
-                Vector2 origin,
-                Vector2 scale,
-                float layerDepth,
-                RectangleF scissor,
-                SpriteOptions options)
-        {
-            origin *= scale;
-
-            var sourceSize = new Vector2(sourceRectangle.Width, sourceRectangle.Height) / textureSize;
-            var srcPos = new Vector2(sourceRectangle.X, sourceRectangle.Y) / textureSize;
-            UV = CreateFlip(options) * Matrix4x4.CreateScale(new Vector3(sourceSize, 1)) * Matrix4x4.CreateTranslation(new(srcPos, 0));
-            Color = ToVector(color);
-            Model =
-                Matrix4x4.CreateScale(new Vector3(new Vector2(sourceRectangle.Width, sourceRectangle.Height) * scale, 1)) *
-                Matrix4x4.CreateTranslation(new Vector3(-origin, 0)) *
-                Matrix4x4.CreateRotationZ(rotation) *
-                Matrix4x4.CreateTranslation(new Vector3(position, layerDepth));
-            Projection = Matrix4x4.Identity;
-            Scissor = scissor;
-        }
-
-        public BatchItem(Vector2 textureSize, 
-            Rectangle destinationRectangle,
-            Rectangle sourceRectangle,
-            Color color,
-            float rotation,
-            Vector2 origin,
-            float layerDepth,
-            RectangleF scissor,
-            SpriteOptions options)
+        public BatchItem(Vector2 textureSize, RectangleF destinationRectangle, RectangleF sourceRectangle, Color color,
+                         float rotation, Vector2 origin, float layerDepth, RectangleF scissor, SpriteOptions options)
         {
             var sourceSize = new Vector2(sourceRectangle.Width, sourceRectangle.Height) / textureSize;
             var pos = new Vector2(sourceRectangle.X, sourceRectangle.Y) / textureSize;
@@ -59,11 +25,23 @@ namespace Peridot
             Scissor = scissor;
         }
 
+        public BatchItem(Vector2 textureSize, Vector2 position, Rectangle sourceRectangle, Color color, float rotation,
+                         Vector2 origin, Vector2 scale, float layerDepth, RectangleF scissor, SpriteOptions options) 
+            : this(textureSize, new RectangleF(position.X, position.Y, sourceRectangle.Width * scale.X, sourceRectangle.Height * scale.Y), 
+                    sourceRectangle, 
+                    color, 
+                    rotation,
+                    origin, 
+                    layerDepth,
+                    scissor, 
+                    options)
+        {
+        }
+
         public Matrix4x4 UV { get; set; }
         public Vector4 Color { get; set; }
         public Matrix4x4 Model { get; set; }
         public Matrix4x4 Projection { get; set; }
-
         public RectangleF Scissor { get; set; }
 
         private static Vector4 ToVector(Color color) => new Vector4(color.R, color.G, color.B, color.A) * (1f / 255f);
